184d46333a68d4e669c852ef5c62e2ec0d6d20c6
uptownkiddo <muchoki.lewis8@gmail.com>
2023-04-25 22:29:27 -0700

    corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,5 +27,4 @@ void bubble_sort(int *array, size_t size)
 			}
 		}
 	}
-} 
-
+}
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,37 +3,35 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *curr, *prev;
 
-    listint_t *curr, *prev;
-
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
-
-    curr = (*list)->next;
-    while (curr != NULL) {
-        prev = curr->prev;
-        while (prev != NULL && prev->n > curr->n) {
-            /* Swap nodes */
-            prev->next = curr->next;
-            if (curr->next != NULL)
-                curr->next->prev = prev;
-
-            curr->next = prev;
-            curr->prev = prev->prev;
-            if (prev->prev != NULL)
-                prev->prev->next = curr;
-            prev->prev = curr;
-
-            /* Move prev pointer back */
-            prev = curr->prev;
-
-            /* If the head node was swapped, update the list pointer */
-            if (prev == NULL)
-                *list = curr;
-
-            /* Print the list after each swap */
-            print_list(*list);
-        }
-        curr = curr->next;
-    }
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+	curr = (*list)->next;
+	while (curr != NULL)
+	{
+		prev = curr->prev;
+		while (prev != NULL && prev->n > curr->n)
+		{
+			/* Swap nodes */
+			prev->next = curr->next;
+			if (curr->next != NULL)
+				curr->next->prev = prev;
+			curr->next = prev;
+			curr->prev = prev->prev;
+			if (prev->prev != NULL)
+				prev->prev->next = curr;
+			prev->prev = curr;
+			/* Move prev pointer back */
+			prev = curr->prev;
+			/* If the head node was swapped, update the list pointer */
+			if (prev == NULL)
+				*list = curr;
+			/* Print the list after each swap */
+			print_list(*list);
+		}
+		curr = curr->next;
+	}
 }
